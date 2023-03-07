4610566c6ea1175ea8d93391cc898bccde4a243a
peteratkau <59958667+peteratkau@users.noreply.github.com>
2023-03-07 08:34:25 +0100

    Uppdaterat ex3

--- F2/ex3.php
+++ F2/ex3.php
@@ -1,5 +1,54 @@
 <?php
 	//Här kommer koden...
+
+	//Skapa variabler med default-värden
+	$disabled = true;
+	$bgColor = "#ffffff";
+	$fgColor = "#000000";
+	$css = "body { color: $fgColor; background-color: $bgColor; }";
+
+	//Användaren har tryckt på submit-knappen btnSend
+	if( isset( $_POST["btnSend"] ) ) {
+
+		//Hämta inkommande data från formuläret input type=color
+		$bgColor = $_POST["backgroundcolor"];
+		$fgColor = $_POST["foregroundcolor"];
+
+		//Skapa två kakor med färgerna
+		setcookie("fgColor", $fgColor, time() + 3600);
+		setcookie("bgColor", $bgColor, time() + 3600);
+
+		//Sätt om variabelvärdena
+		$css = "body { color: $fgColor; background-color: $bgColor; }";
+		$disabled = false;
+
+	}
+
+	//Användaren har tryckt på submit-knappen btnReset OCH kakorna kommer till servern
+	if( isset( $_POST["btnReset"] && isset( $_COOKIE["bgColor"]) && isset( $_COOKIE["fgColor"] ) ) {
+
+		//Radera kakorna
+		setcookie("fgColor", "", time() - 3600);
+		setcookie("bgColor", "", time() - 3600);
+
+	}
+
+	//Användaren har inte tryckt på varken btnSend eller btnReset men i requesten till servern kommer 
+	//kakorna bgColor och fgColor med.
+	if( !isset( $_POST["btnSend"] ) && 
+		!isset( $_POST["btnReset"] ) && 
+		isset( $_COOKIE["bgColor"] ) && 
+		isset( $_COOKIE["fgColor"]) ) {
+
+		//Hämta data från kakorna
+		$bgColor = $_COOKIE["bgColor"];
+		$fgColor = $_COOKIE["fgColor"];
+
+		//Sätt om variabelvärden
+		$css = "body { color: $fgColor; background-color: $bgColor; }";
+		$disabled = false;
+	}
+
 ?>
 <!doctype html>
 <html lang="en" >
@@ -18,22 +67,23 @@
 			
 			<form action="<?php echo( $_SERVER["PHP_SELF"] ); ?>" method="post">
 		
-				<input type="color" name="backgroundcolor" value="<?php if( isset( $bgColor )) { echo( $bgColor ); } ?>" />
-				<input type="color" name="foregroundcolor" value="<?php if( isset( $fgColor )) { echo( $fgColor ); } ?>"/>
+				<input type="color" name="backgroundcolor" value="<?php if( isset( $bgColor )) { echo( $bgColor ); } ?>" > <!-- Skriv ut färgvärdet -->
+				<input type="color" name="foregroundcolor" value="<?php if( isset( $fgColor )) { echo( $fgColor ); } ?>" > <!-- Skriv ut färgvärdet -->
 
-				<input type="submit" name="btnSend" value="Send" />
-				<input type="submit" name="btnReset" value="Reset" <?php if($disabled) { echo("disabled"); } ?>/>
+				<input type="submit" name="btnSend" value="Send" >
+				<input type="submit" name="btnReset" value="Reset" <?php if($disabled) { echo("disabled"); } ?> > <!-- Skriv ut disabled om true -->
 			
 			</form>
 		
 			<?php
 			
+				//Utskrifter
 				echo("<p>\$_POST</p>") . PHP_EOL;
 				echo( "<pre>" );
 				print_r( $_POST );
 				echo( "</pre>" );
 
-				echo("<p> \$_SESSION</p>") . PHP_EOL;
+				echo("<p> \$_COOKIE</p>") . PHP_EOL;
 				echo( "<pre>" );
 				print_r( $_COOKIE );
 				echo( "</pre>" );
